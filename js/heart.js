6016854a67d1e1b3f022553ebaa09f6a2b054f10
SaraRFerrer <sararferrer@hotmail.com>
2022-03-25 15:18:42 +0100

    cart

--- js/heart.js
+++ js/heart.js
@@ -11,3 +11,34 @@ heart.addEventListener("click", function onClick() {
 
 });
 
+
+const cartCounter = document.querySelector(".counter");
+const cartButton = document.querySelector(".cta-cart");
+let cartArray =[]
+
+function loadCartCounter() {
+  let productCounter = localStorage.getItem("cartNumber");
+
+  if (productCounter) {
+    cartCounter.textContent = productCounter;
+  }
+}
+
+loadCartCounter();
+
+cartButton.addEventListener("click", addAmount);
+
+function addAmount() {
+  let productCounter = localStorage.getItem("cartNumber");
+
+  productCounter = parseInt(productCounter);
+
+  if (productCounter) {
+    localStorage.setItem("cartNumber", productCounter + 1);
+    cartCounter.textContent = productCounter + 1;
+  } else {
+    localStorage.setItem("cartNumber", 1);
+    cartCounter.textContent = 1;
+  }
+
+}
